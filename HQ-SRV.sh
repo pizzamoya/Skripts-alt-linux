b2a50a944c828928f47973cb618747d98e5d504b
Alex <147999158+AklisTib@users.noreply.github.com>
2024-04-05 09:06:13 +0400

    Update HQ-SRV.sh

--- HQ-SRV.sh
+++ HQ-SRV.sh
@@ -213,11 +213,6 @@ named-checkconf -z
 
 systemctl restart bind
 
-host hq-r.hq.work
-host hq-srv.hq.work
-host 192.168.100.62
-host 192.168.200.14
-
 timedatectl set-timezone Europe/Moscow
 
 cat <<EOF > /etc/chrony.conf
@@ -230,3 +225,30 @@ server 2000:100::3f iburst
 EOF
 
 systemctl enable --now chronyd
+
+control bind-chroot disabled
+
+grep -q 'bind-dns' /etc/bind/named.conf || echo 'include "/var/lib/samba/bind-dns/named.conf";' >> /etc/bind/named.conf
+
+sed -i '8a\tkey-gssapi-keytab "/var/lib/samba/bind-dns/dns.keytab";' /etc/bind/options.conf
+sed -i '9a\minimal-responses yes;' /etc/bind/options.conf
+sed -i '91a\category lame-servers {null;};' /etc/bind/options.conf
+systemctl stop bind
+
+sed -i 's/HOSTNAME=ISP/HOSTNAME=hq-srv.demo.first/g' /etc/sysconfig/network
+
+hostnamectl set-hostname hq-srv.demo.first; exec bash
+domainname demo.first
+
+rm -f /etc/samba/smb.conf
+rm -rf /var/lib/samba
+rm -rf /var/cache/samba
+mkdir -p /var/lib/samba/sysvol
+
+samba-tool domain provision --realm=demo.first --domain=demo --adminpass='P@ssw0rd' --dns-backend=BIND9_DLZ --server-role=dc --use-rfc2307
+
+systemctl enable --now samba
+systemctl enable --now bind
+cp /var/lib/samba/private/krb5.conf /etc/krb5.conf
+
+samba-tool domain info 127.0.0.1
